13c2e3573c783f828f6993e16a2d90d5c110bd7c
koomekevo <koome.kevo@gurucrafts.net>
2022-07-10 19:16:11 +0300

    Test MongoDB connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-dotenv.config();
-const MongoClient = mongodb.MongoClient;
+dotenv.config()
+const MongoClient = mongodb.MongoClient
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 
 MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
-  poolSize: 50,
-  wtimeout: 2500,
-  useNewUrlParse: true,
+  wtimeoutMS: 2500,
 })
 .catch(err => {
   console.error(err.stack);
