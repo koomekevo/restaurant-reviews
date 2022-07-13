ac674f78e614ef2945f145d28b219217ad333f3f
koomekevo <koome.kevo@gurucrafts.net>
2022-07-13 17:04:14 +0300

    Modify .eslintrc.json and linters.yml

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import RestaurantsCtrl from './api/restaurants.controller.js';
+import RestaurantsCtrl from './restaurants.controller.js';
 
 const router = express.Router();
 
